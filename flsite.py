5cc5ca1ff23727dc316d9ad7d3978606036a946b
slavenbar <gazflair3d@gmail.com>
2020-12-23 14:00:45 +0300

    new com

--- flsite.py
+++ flsite.py
@@ -2,40 +2,44 @@ from flask import Flask,render_template,url_for,request,flash,session,redirect,a
 import sqlite3
 import os
 from FDataBase import FDataBase
+from flask_sqlalchemy import SQLAlchemy
 
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
 SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
 
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+#Закоментировать код ctrl+/
+#app.config.from_object(__name__)
 
-app.config.update(dict(DATABASE=os.path.join(app.root_path,'/tmp/flsite.db')))
+#app.config.update(dict(DATABASE=os.path.join(app.root_path,'/tmp/flsite.db')))
 
-def connect_db():
-    conn = sqlite3.connect(app.config['DATABASE'])
-    conn.row_factory = sqlite3.Row
-    return conn
+# def connect_db():
+#     conn = sqlite3.connect(app.config['DATABASE'])
+#     conn.row_factory = sqlite3.Row
+#     return conn
 
-def create_db():
-    """Вспомогательная функция для создания таблиц БД"""
-    db = connect_db()
-    with app.open_resource('sq_db.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-    db.close()
-
-def get_db():
-    '''Соединение с БД, если оно еще не установлено'''
-    if not hasattr(g, 'link_db'):
-        g.link_db = connect_db()
-    return g.link_db
-
-@app.teardown_appcontext
-def close_db(error):
-    '''Закрываем соединение с БД, если оно было установлено'''
-    if hasattr(g, 'link_db'):
-        g.link_db.close()
+# def create_db():
+#     """Вспомогательная функция для создания таблиц БД"""
+#     db = connect_db()
+#     with app.open_resource('sq_db.sql', mode='r') as f:
+#         db.cursor().executescript(f.read())
+#     db.commit()
+#     db.close()
+#
+# def get_db():
+#     '''Соединение с БД, если оно еще не установлено'''
+#     if not hasattr(g, 'link_db'):
+#         g.link_db = connect_db()
+#     return g.link_db
+#
+# @app.teardown_appcontext
+# def close_db(error):
+#     '''Закрываем соединение с БД, если оно было установлено'''
+#     if hasattr(g, 'link_db'):
+#         g.link_db.close()
 
 #app.config['SECRET_KEY'] = 'fdgdfgdfggf786hfg6hfg6h7f'
 menu = [{'name':'Главная', 'url': 'main-app'},
@@ -111,6 +115,7 @@ def login():
 
     return render_template('login.html', title="Авторизация", menu=menu)
 
+#Декоратор для отправки ошибки
 @app.errorhandler(404)
 def pageNotFount(error):
     return render_template('page404.html', title="Страница не найдена", menu=menu)
